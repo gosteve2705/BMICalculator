b74dc4f14d5d5fab3ede1901d7cfca4d6b38b6d4
gosteve <nomuojamaro@gmail.com>
2020-06-02 21:28:03 +0100

    bmi calculator

---
+++ bmicalculator.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/', function(req,res){
+  var weight = req.body.weight ;
+  var height = req.body.height ;
+  height = height/100;
+
+  var bmi = weight / Math.pow(height, 2);
+  bmi = bmi.toFixed(2);
+  
+  res.send(`Your BMI is ${bmi} Kg/(m)2` );});
+
+app.listen(3000);
